4e8d12ed4e99fdf13d897d91a477758217fa7ddb
Samantha-Ruth <sam.haberman@gmail.com>
2023-09-13 14:08:49 -0600

    finished javascript logic to connect timer to border and cleaned up the style

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const durationInput = document.querySelector("#duration");
 const startButton = document.querySelector("#start");
 const pauseButton = document.querySelector("#pause");
+const circle = document.querySelector('circle');
 
+const perimeter = circle.getAttribute('r') * 2 * Math.PI;
+circle.setAttribute('stroke-dasharray', perimeter);
+
+let duration;
 const timer = new Timer(durationInput, startButton, pauseButton, {
-  onStart() {
+  onStart(totalDuration) {
+    duration = totalDuration;
     console.log("Timer Started");
   },
-  onTick() {
-    console.log("Timer just ticked down");
+  onTick(timeRemaining) {
+    // remove sections of the perimeter as time passes
+    circle.setAttribute('stroke-dashoffset', 
+    // set the stroke-dashoffset to the following math:
+    perimeter * timeRemaining / duration - perimeter);
   },
   onComplete() {
     console.log("Timer completed!");
